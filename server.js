95405c7fec80513b589087012281e00ffcb32ab3
FacuA0 <facundoamarilla28@gmail.com>
2023-05-15 01:06:32 -0300

    Replaced express with http and made a tables link

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 let mysql = require("mysql");
 let process = require("process");
-let express = require("express");
+let http = require("http");
 
-let app = express();
 
 let options = {
     host: "localhost",
@@ -10,30 +9,67 @@ let options = {
     password: ""
 }
 
-app.get("/", (req, res) => {
+let server = http.createServer((req, res) => {
+
+    if (req.url == "/") {
+        showDatabases(res);
+    }
+    else {
+        showTablesInDatabase(res, req.url.slice(1));
+    }
+    
+});
+
+server.listen(9040);
+
+function showDatabases(res) {
     let connection = mysql.createConnection(options);
     connection.connect();
     connection.query("SHOW DATABASES;", (error, results, fields) => {
         if (error) {
             console.log("Error while querying.");
-            res.send("Error with query.");
+            res.end("Error with query.");
             return;
         }
 
-        let response = "<table><caption>Databases</caption><th>Databases</th>";
+        res.write("<table border='1'><caption>Databases</caption><tr><th>Databases</th></tr>");
         for (row of results) {
             let fieldName;
             for (key in row) {
                 fieldName = key;
             }
 
-            response += "<tr><td>" + row[fieldName] + "</td></tr>";
+            let field = row[fieldName];
+            res.write("<tr><td><a href='./" + field + "'>" + field + "</a></td></tr>");
         }
-        response += "</table>";
+        res.write("</table>");
 
-        res.send(response);
+        res.end();
     });
     connection.end();
-});
+}
 
-app.listen(9040);
+function showTablesInDatabase(res, database) {
+    let connection = mysql.createConnection(Object.assign({}, options, {database}));
+    connection.connect();
+    connection.query("SHOW TABLES;", (error, results, fields) => {
+        if (error) {
+            console.log("Error while querying.");
+            res.end("Error with query.");
+            return;
+        }
+
+        res.write("<table border='1'><caption>Tables</caption><tr><th>Tables</th></tr>");
+        for (row of results) {
+            let fieldName;
+            for (key in row) {
+                fieldName = key;
+            }
+
+            res.write("<tr><td>" + row[fieldName] + "</td></tr>");
+        }
+        res.write("</table>");
+        res.end();
+    });
+    connection.end();
+}
